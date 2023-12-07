03b1f7e7808e194fa4a77ae8b92ba6a8f3cfcbae
Joel Jativa <jodajativa17@gmail.com>
2023-12-06 20:48:35 -0500

    avances

---
+++ js/responsiveMenu.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.getElementById('menu')
+    const navMenu = document.querySelector('.nav--menu')
+    menu.addEventListener('click', function(){
+        navMenu.classList.toggle('transalate--menu')
+    })
+})
