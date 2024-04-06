78f951afe580ed920b1512a0ca52c967a527835d
Joel Jativa <jodajativa17@gmail.com>
2024-04-06 14:24:30 -0500

    portafolioJS

--- js/changeLenguage.js
+++ js/changeLenguage.js
@@ -14,14 +14,14 @@ document.addEventListener('DOMContentLoaded', function () {
           const translation = phrase.getAttribute('data-translate-en');
           phrase.textContent = translation
         }
-        document.getElementById('downloadCV').href = '/cv/cv_en.png'
+        document.getElementById('downloadCV').href = '/cv/Currículum Vitae Cv English.pdf'
         language = 'en'
       } else {
         for (const phrase of elementsToTransalate) {
           const translation = phrase.getAttribute('data-translate-es');
           phrase.textContent = translation
         }
-        document.getElementById('downloadCV').href = '/cv/cv_es.png'
+        document.getElementById('downloadCV').href = '/cv/Currículum Vitae Cv Español.pdf'
         language = 'es'
   
       }
